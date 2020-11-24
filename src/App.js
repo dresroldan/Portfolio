85aafd9b1e005f7ed0d23105a05f945c9276609b
Andres Roldan <dresroldan@gmail.com>
2020-11-24 13:06:03 -0600

    removed react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import "./App.css";
 import Header from "./components/Header";
-import StickyFooter from "./components/StickyFooter";
-import About from "./components/About"
+
 
 function App() {
   return (
     <div className="App">
-      <Header />
-<About/>
-      <StickyFooter />
+
     </div>
   );
 }
