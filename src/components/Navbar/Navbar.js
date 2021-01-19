814bebaf3a2d1a445519fb201e3f8bb9eb43f8e6
Andres Roldan <dresroldan@gmail.com>
2021-01-19 16:17:05 -0600

    refactored navbar, added spacing between links

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,20 +1,27 @@
 import React from "react";
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  makeStyles,
-  Link,
-} from "@material-ui/core";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import Link from "@material-ui/core/Link";
+import { makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   header: {
     backgroundColor: "transparent",
     color: "black",
     boxShadow: "0px 0px 0px 0px",
-    marginTop: "10vh",
+    marginTop: "5vh",
   },
   projectsLink: { marginRight: theme.spacing(2) },
+
+  toolbar: {
+    justifyContent: "space-between",
+  },
+
+  links: {
+    display: "flex",
+  },
 }));
 
 export default function Navbar() {
@@ -22,18 +29,22 @@ export default function Navbar() {
   const preventDefault = (event) => event.preventDefault();
   return (
     <AppBar className={classes.header} position="relative">
-      <Toolbar>
-        <Typography variant="h6" noWrap className={classes.projectsLink}>
-          <Link href="#applications">Projects</Link>
-        </Typography>
+      <Toolbar className={classes.toolbar}>
+        <Box>
+          <Typography variant="h6" noWrap className={classes.projectsLink}>
+            <Link href="#applications">Projects</Link>
+          </Typography>
+        </Box>
 
-        <Typography variant="h6" noWrap className={classes.projectsLink}>
-          <Link href="#applications">About</Link>
-        </Typography>
+        <Box className={classes.links}>
+          <Typography variant="h7" noWrap className={classes.projectsLink}>
+            <Link href="#applications">About</Link>
+          </Typography>
 
-        <Typography variant="h6" noWrap className={classes.projectsLink}>
-          <Link href="#applications">Contact</Link>
-        </Typography>
+          <Typography variant="h7" noWrap >
+            <Link href="#applications">Contact</Link>
+          </Typography>
+        </Box>
       </Toolbar>
     </AppBar>
   );
