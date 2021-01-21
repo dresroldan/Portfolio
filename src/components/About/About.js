d30a6f85de4a44ae398a5dc190551817f4a75009
Andres Roldan <dresroldan@gmail.com>
2021-01-20 23:48:52 -0600

    added border around about me section and aligned text

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,6 +4,7 @@ import Avatar from "../Avatar/Avatar";
 import Button from "@material-ui/core/Button";
 import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
+import Fade from "react-reveal/Fade";
 
 const useStyles = makeStyles((theme) => ({
   aboutContent: {
@@ -12,6 +13,11 @@ const useStyles = makeStyles((theme) => ({
     border: "1px solid",
     borderRadius: "20px",
     marginTop: "20px",
+    boxShadow: "#000 1px 1px 0",
+    boxShadow: "#000 2px 2px 0",
+    boxShadow: "#000 3px 3px 0",
+    boxShadow: "#000 4px 4px 0",
+    boxShadow: "#000 4px 5px 0",
   },
 
   aboutContainer: {
@@ -27,54 +33,71 @@ export default function About() {
   const classes = useStyles();
 
   return (
-    <div className={classes.aboutContent}>
-      <Grid container className={classes.aboutContainer}>
-        <Grid item xs={12} md={5}>
-          <Avatar className={classes.avatar} />
+    <Fade duration={2500} left>
+      <div className={classes.aboutContent}>
+        
+        
+        <Grid container className={classes.aboutContainer}>
+          <Grid item xs={12} md={5} >
+            <Avatar className={classes.avatar} />
+          </Grid>
+
+          <Grid item xs={12} md={7}>
+            <Typography component="h2" variant="h6" gutterBottom>
+              <Box fontStyle="oblique" fontWeight={700} textAlign="left" m={1}>
+                About Me
+              </Box>
+            </Typography>
+
+            <Typography variant="h7" paragraph>
+              <Box textAlign="left" m={1}>
+                Creative and dependable front-end web developer with an
+                outstanding customer service record, and superb work ethic. Fast
+                learner, hard worker, and team player who is proficient in an
+                array of scripting languages and multimedia web tools.
+              </Box>
+            </Typography>
+
+            <Typography variant="h6" gutterBottom>
+              <Box fontStyle="oblique" fontWeight={700} textAlign="left" m={1}>
+                Contact
+              </Box>
+            </Typography>
+
+            <Typography variant="h8" gutterBottom>
+              <Box textAlign="left" m={1}>
+                Andrés Roldán
+              </Box>
+              <Box textAlign="left" m={1}>
+                Chicago, IL
+              </Box>
+              <Box textAlign="left" m={1}>
+                779.207.8144
+              </Box>
+              <Box textAlign="left" m={1}>
+                dresroldan@gmail.com
+              </Box>
+            </Typography>
+
+           
+           
+            <Box mt={4} ml={1}>
+              <a
+                href="https://drive.google.com/file/d/1uWGIeu_o2xWPSQHOJcNfhrtt3uzjUwqm/view"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Button size="large" variant="contained">
+                  Resume
+                </Button>
+              </a>
+            </Box>
+         
+         
+         
+          </Grid>
         </Grid>
-
-        <Grid item xs={12} md={7}>
-          <Typography component="h2" variant="h6" gutterBottom>
-            About Me
-          </Typography>
-          <Typography variant="h7" paragraph>
-            Creative and dependable front-end web developer with an outstanding
-            customer service record, and superb work ethic. Fast learner, hard
-            worker, and team player who is proficient in an array of scripting
-            languages and multimedia web tools.
-          </Typography>
-
-          <Typography variant="h6" gutterBottom>
-            Contact
-          </Typography>
-
-          <Box>
-            <Typography variant="h7" gutterBottom>
-              Andrés Roldán
-            </Typography>
-            <Typography variant="h7" gutterBottom>
-              Chicago, IL
-            </Typography>
-            <Typography variant="h7" gutterBottom>
-              779.207.8144
-            </Typography>
-            <Typography variant="h7" gutterBottom>
-              dresroldan@gmail.com
-            </Typography>
-
-            <a
-              href="https://drive.google.com/file/d/1uWGIeu_o2xWPSQHOJcNfhrtt3uzjUwqm/view"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={classes.textPadding}
-            >
-              <Button size="large" variant="contained">
-                Resume
-              </Button>
-            </a>
-          </Box>
-        </Grid>
-      </Grid>
-    </div>
+      </div>
+    </Fade>
   );
 }
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -10,24 +10,19 @@ import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import InstagramIcon from "@material-ui/icons/Instagram";
 import Fade from "react-reveal/Fade";
-import "./Hero.css"
+// import Fab from "@material-ui/core/Fab";
+// import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
 
 const useStyles = makeStyles((theme) => ({
   heroContent: {
-    // height:1000
     padding: theme.spacing(30, 0, 30),
-    
-    background:'-webkit-radial-gradient(circle, hsla(191, 60%, 89%, 1) 0%, hsla(56, 59%, 86%, 1) 50%, hsla(25, 85%, 69%, 1) 100%);',
-
-    borderRadius:'20px'
-  
-  
+    background:
+      "-webkit-radial-gradient(circle, hsla(191, 60%, 89%, 1) 0%, hsla(56, 59%, 86%, 1) 50%, hsla(25, 85%, 69%, 1) 100%);",
+    borderRadius: "20px",
   },
-
   heading: {
     paddingBottom: "10px",
   },
-
   icons: {
     paddingTop: "25px",
   },
@@ -37,9 +32,9 @@ export default function Hero() {
   const classes = useStyles();
 
   return (
-    <div className={classes.heroContent} >
-      <Container  maxWidth="sm">
-        <Fade top cascade>
+    <div className={classes.heroContent}>
+      <Container maxWidth="sm">
+        <Fade duration={2000} bottom cascade>
           <Typography
             className={classes.heading}
             variant="h5"
@@ -48,48 +43,59 @@ export default function Hero() {
           >
             Hello, I'm Andrés{" "}
           </Typography>
+          {/* <Fade top cascade> */}
+          <Typography variant="h4" align="center" color="primary" paragraph>
+            I am a developer who focuses on building applications that improves
+            lives through user experience.
+          </Typography>
         </Fade>
-        {/* <Fade top cascade> */}
-        <Typography variant="h4" align="center" color="primary" paragraph>
-          I am a developer who focuses on building applications that improves lives
-          through user experience.
-        </Typography>
         {/* </Fade> */}
         <div className={classes.links}>
-          <Grid container spacing={0} justify="center">
-            <Grid item>
-              <IconButton
-                aria-label="Linkedin.com"
-                onClick={() =>
-                  window.open("https://www.linkedin.com/in/andresroldan23/")
-                }
-              >
-                <LinkedInIcon fontSize="large" />
-              </IconButton>
-            </Grid>
+          <Fade duration={2000} bottom cascade>
+            <Grid container spacing={0} justify="center">
+              <Grid item>
+                <IconButton
+                  aria-label="Linkedin.com"
+                  onClick={() =>
+                    window.open("https://www.linkedin.com/in/andresroldan23/")
+                  }
+                >
+                  <LinkedInIcon fontSize="large" />
+                </IconButton>
+              </Grid>
 
-            <Grid item>
-              <IconButton
-                aria-label="Github.com"
-                onClick={() => window.open("https://github.com/dresroldan")}
-              >
-                <GitHubIcon fontSize="large" />
-              </IconButton>
-            </Grid>
+              <Grid item>
+                <IconButton
+                  aria-label="Github.com"
+                  onClick={() => window.open("https://github.com/dresroldan")}
+                >
+                  <GitHubIcon fontSize="large" />
+                </IconButton>
+              </Grid>
 
-            <Grid item>
-              <IconButton
-                aria-label="Linkedin.com"
-                onClick={() =>
-                  window.open("https://www.instagram.com/mar_de_luz_/")
-                }
-              >
-                <InstagramIcon fontSize="large" />
-              </IconButton>
+              <Grid item>
+                <IconButton
+                  aria-label="Linkedin.com"
+                  onClick={() =>
+                    window.open("https://www.instagram.com/mar_de_luz_/")
+                  }
+                >
+                  <InstagramIcon fontSize="large" />
+                </IconButton>
+              </Grid>
             </Grid>
-          </Grid>
+          </Fade>
         </div>
+
+        {/* <Grid container spacing={0} justify="center">
+          <Fab backgroundColor="#A80202">
+            <KeyboardArrowDownIcon />
+          </Fab>
+        </Grid> */}
       </Container>
+   
+   
+   
     </div>
   );
 }
