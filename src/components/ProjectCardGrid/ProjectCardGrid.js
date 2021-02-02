89cb28073119c41827b7c37cf802db0034af7080
Andres Roldan <dresroldan@gmail.com>
2021-02-02 10:45:04 -0600

    added dermpal project to project section

--- src/components/ProjectCardGrid/ProjectCardGrid.js
+++ src/components/ProjectCardGrid/ProjectCardGrid.js
@@ -6,9 +6,8 @@ import ProjectCard from "../ProjectCard/ProjectCard";
 import firstImage from "../../images/Projects/picniqapp.png";
 import secondImage from "../../images/Projects/habit1.png";
 import thirdImage from "../../images/Projects/weatherdashboard.png";
-import fourthImage from "../../images/Projects/scheduler.png";
-import fifthImage from "../../images/Projects/fitnessmain.png";
-import sixthImage from "../../images/Projects/budget-dashboard.png";
+import fourthImage from "../../images/Projects/dermpal.png";
+
 import { Typography } from "@material-ui/core";
 import Box from "@material-ui/core/Box";
 const useStyles = makeStyles((theme) => ({
@@ -18,7 +17,7 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: "column",
   },
   media: {
-    height: 140,
+    height: 150,
   },
   cardGrid: {
     // paddingTop: theme.spacing(8),
@@ -31,6 +30,15 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const cards = [
+  {
+    id: 4,
+    image: fourthImage,
+    title: "DermPal",
+    description:
+      "DermPal is a barcode scanning app that is designed to help the user find skincare products that are compatible with their skin.",
+    demoLink: "https://dermpal1.herokuapp.com/",
+    codeLink: "https://github.com/Bbeyenene/dermpal1",
+  },
   {
     id: 1,
     image: firstImage,
@@ -57,34 +65,6 @@ const cards = [
     demoLink: "https://dresroldan.github.io/weather-dashboard/",
     codeLink: "https://github.com/dresroldan/weather-dashboard",
   },
-  {
-    id: 4,
-    image: fourthImage,
-    title: "Work Day Scheduler",
-    description:
-      "This application is a calendar that allows the user to save events for each standard business hour (9am-5pm).",
-    demoLink: "https://dresroldan.github.io/work-day-scheduler/",
-    codeLink: "https://github.com/dresroldan/work-day-scheduler",
-  },
-
-  // {
-  //   id: 5,
-  //   image: fifthImage,
-  //   title: "Workout Tracker",
-  //   description: "fjsdhfjksdhf",
-  //   demoLink:
-  //     "https://limitless-woodland-88147.herokuapp.com/?id=5fa1f26b8d9f8f0017c7740e",
-  //   codeLink: "https://github.com/dresroldan/workout-tracker",
-  // },
-  // {
-  //   id: 6,
-  //   image: sixthImage,
-  //   title: "PWA Budget Tracker",
-  //   description:
-  //     "This application allows users to add expenses and deposits to their budget with or without an online connection using Progressive Web Application (PWA).",
-  //   demoLink: "https://pwa-budget-tracker-andres.herokuapp.com/",
-  //   codeLink: "https://github.com/dresroldan/pwa-budget-tracker",
-  // },
 ];
 
 export default function MediaCard() {
